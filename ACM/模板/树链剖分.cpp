342632ae020227cb19a0af2cb7b7a1785f7f8c15
CFRFREE <60842418+CFRFREE@users.noreply.github.com>
2022-07-22 11:21:58 +0800

    master

--- ACM/模板/树链剖分.cpp
+++ ACM/模板/树链剖分.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
-#define LL long long
-#define maxn 100005
+#define int long long
+#define N 100005
 using namespace std;
-int n, m, r, P, id[maxn], fa[maxn], son[maxn], cnt;
-int top[maxn], sum, a[maxn], b[maxn], deep[maxn], size[maxn];
-vector<int>v[maxn];
+int n, m, r, P, id[N], fa[N], son[N], cnt;
+int top[N], sum, a[N], b[N], deep[N], sz[N];
+vector<int>v[N];
 struct node {
 	int l, r, w, f;
-} tree[maxn << 2];
+} tree[N << 2];
 inline int read()
 {
 	int X = 0, w = 0; char ch = 0;
@@ -18,65 +18,62 @@ inline int read()
 
 void PushUp(int k)
 {
-	tree[k].w = tree[k << 1].w + tree[k << 1 | 1].w;
-	tree[k].w %= P;
+	tree[k].w = (tree[k << 1].w + tree[k << 1 | 1].w) % P;
 }
 
 void PushDown(int k)
 {
-	if (tree[k].f)
-	{
-		int x = tree[k].f;
-		tree[k].f = 0;
-		tree[k << 1].f += x;
-		tree[k << 1 | 1].f += x;
-		tree[k << 1].w += x * (tree[k << 1].r - tree[k << 1].l + 1);
-		tree[k << 1 | 1].w += x * (tree[k << 1 | 1].r - tree[k << 1 | 1].l + 1);
-		tree[k << 1].w %= P;
-		tree[k << 1 | 1].w %= P;
-	}
+	if (!tree[k].f)return;
+	int x = tree[k].f;
+	tree[k].f = 0;
+	tree[k << 1].f += x;
+	tree[k << 1 | 1].f += x;
+	tree[k << 1].w += x * (tree[k << 1].r - tree[k << 1].l + 1);
+	tree[k << 1 | 1].w += x * (tree[k << 1 | 1].r - tree[k << 1 | 1].l + 1);
+	tree[k << 1].w %= P;
+	tree[k << 1 | 1].w %= P;
 }
 
-void build(int k, int ll, int rr)
+void build(int k, int L, int R)
 {
-	tree[k].l = ll;
-	tree[k].r = rr;
-	if (ll == rr)
+	tree[k].l = L;
+	tree[k].r = R;
+	if (L == R)
 	{
-		tree[k].w = a[ll];
+		tree[k].w = a[L];
 		return;
 	}
-	int mid = (ll + rr) >> 1;
-	build(k << 1, ll, mid);
-	build(k << 1 | 1, mid + 1, rr);
+	int mid = (L + R) >> 1;
+	build(k << 1, L, mid);
+	build(k << 1 | 1, mid + 1, R);
 	PushUp(k);
 }
 
-void change(int k, int ll, int rr, int d)
+void change(int k, int L, int R, int d)
 {
-	if (tree[k].l >= ll && tree[k].r <= rr)
+	if (tree[k].l >= L && tree[k].r <= R)
 	{
 		tree[k].f += d;
 		tree[k].w += d * (tree[k].r - tree[k].l + 1);
+		tree[k].w %= P;
 		return;
 	}
 	PushDown(k);
 	int mid = (tree[k].l + tree[k].r) >> 1;
-	if (mid >= ll)change(k << 1, ll, rr, d);
-	if (rr > mid)change(k << 1 | 1, ll, rr, d);
+	if (mid >= L)change(k << 1, L, R, d);
+	if (R > mid)change(k << 1 | 1, L, R, d);
 	PushUp(k);
 }
 
-int search(int k, int ll, int rr)
+int search(int k, int L, int R)
 {
 	int ans = 0;
-	if (tree[k].l >= ll && tree[k].r <= rr)
+	if (tree[k].l >= L && tree[k].r <= R)
 		return tree[k].w % P;
-
 	PushDown(k);
 	int mid = (tree[k].l + tree[k].r) >> 1;
-	if (mid >= ll)ans += search(k << 1, ll, rr);
-	if (rr > mid)ans += search(k << 1 | 1, ll, rr);
+	if (mid >= L)ans += search(k << 1, L, R);
+	if (R > mid)ans += search(k << 1 | 1, L, R);
 	return ans % P;
 }
 
@@ -84,20 +81,19 @@ int dfs1(int x, int f, int dep)
 {
 	deep[x] = dep;
 	fa[x] = f;
-	size[x] = 1;
+	sz[x] = 1;
 	int maxson = -1;
-	for (int i = 0; i < v[x].size(); i++)
+	for (auto y : v[x])
 	{
-		int y = v[x][i];
 		if (y == fa[x])continue;
-		size[x] += dfs1(y, x, dep + 1);
-		if (size[y] > maxson)
+		sz[x] += dfs1(y, x, dep + 1);
+		if (sz[y] > maxson)
 		{
 			son[x] = y;
-			maxson = size[y];
+			maxson = sz[y];
 		}
 	}
-	return size[x];
+	return sz[x];
 }
 
 void dfs2(int x, int topf)
@@ -105,14 +101,11 @@ void dfs2(int x, int topf)
 	top[x] = topf;
 	id[x] = ++cnt;
 	a[cnt] = b[x];
-
 	if (!son[x])return;
-
 	dfs2(son[x], topf);
-
-	for (int i = 0; i < v[x].size(); i++)
-		if (!id[v[x][i]])
-			dfs2(v[x][i], v[x][i]);
+	for (auto y : v[x])
+		if (!id[y])
+			dfs2(y, y);
 }
 
 void path_change(int x, int y, int z)
@@ -139,19 +132,20 @@ void path_search(int x, int y)
 	}
 	if (deep[x] > deep[y])swap(x, y);
 	ans += search(1, id[x], id[y]);
-	cout << ans % P << endl;
+	printf("%Ld\n", ans % P);
 }
 
 void tree_change(int x, int y)
 {
-	change(1, id[x], id[x] + size[x] - 1, y);
+	change(1, id[x], id[x] + sz[x] - 1, y);
 }
 
 void tree_search(int x)
 {
-	cout << search(1, id[x], id[x] + size[x] - 1) << endl;
+	printf("%Ld\n", search(1, id[x], id[x] + sz[x] - 1));
 }
-int main()
+
+signed main()
 {
 	n = read(), m = read(), r = read(), P = read();
 	for (int i = 1; i <= n; i++)
@@ -162,31 +156,28 @@ int main()
 		v[x].push_back(y);
 		v[y].push_back(x);
 	}
-
 	dfs1(r, 0, 1);
-
 	dfs2(r, r);
 	build(1, 1, n);
-
 	for (int i = 1; i <= m; i++)
 	{
-		int p = read();
-		if (p == 1)
+		int opt = read();
+		if (opt == 1)
 		{
 			int x = read(), y = read(), z = read();
 			path_change(x, y, z);
 		}
-		if (p == 2)
+		else if (opt == 2)
 		{
 			int x = read(), y = read();
 			path_search(x, y);
 		}
-		if (p == 3)
+		else if (opt == 3)
 		{
 			int x = read(), y = read();
 			tree_change(x, y);
 		}
-		if (p == 4)
+		else
 		{
 			int x = read();
 			tree_search(x);
@@ -194,4 +185,3 @@ int main()
 	}
 	return 0;
 }
-
